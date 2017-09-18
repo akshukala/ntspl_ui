f3c1fda2c7025e1cb7e76a1f4696cc72b2a386cc
Akshay <akshay@nimbrisk.com>
2017-09-18 10:44:31 +0530

    production file created

---
+++ ntspl_ui/settings/production.py
@@ -0,0 +1,6 @@
+
+from nt_db.settings.local import *
+from ntspl_ui.settings.base import *
+
+USER_SERVICE_URL = 'http://35.154.199.148:9861/userservice/'
+TRAFFIC_SERVICE_URL = 'http://35.154.199.148:9862/trafficservice/'
