98f5144bb87da6f0bba5a2adcf31358ee4f2d8d3
Akshay <akshay@nimbrisk.com>
2017-09-18 16:36:00 +0530

    production file change

--- ntspl_ui/settings/production.py
+++ ntspl_ui/settings/production.py
@@ -2,5 +2,5 @@
 from nt_db.settings.production import *
 from ntspl_ui.settings.base import *
 
-USER_SERVICE_URL = 'http://35.154.199.148:9861/userservice/'
-TRAFFIC_SERVICE_URL = 'http://35.154.199.148:9862/trafficservice/'
+USER_SERVICE_URL = 'http://0.0.0.0:9861/userservice/'
+TRAFFIC_SERVICE_URL = 'http://0.0.0.0:9862/trafficservice/'
